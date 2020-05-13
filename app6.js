08a53e8a01bcabe078c80a421d4da1be7a66b41e
eugene.antonevich <eugene.antonevich@8base.com>
2020-05-13 20:52:21 +0300

    add comment

--- app6.js
+++ app6.js
@@ -2,9 +2,37 @@ const express=require("express");
 const url = require("url");
 const _ = require("lodash");
 
+// про функции
+/*
+  Есть два вида функций (в js)
+  стрелочные
+    const some_function = async () => {
+      ...
+    }
+
+  и через слово function
+    function test_function() {
+      ...
+    }
+
+  разница между ними в том что у стрелочной не будет this.
+
+  для просты используй всегда стрелоную, this в функциях - это бред
+
+  про this потом еще
+
+  каждая функция может возврашать какие-то данные
+
+  const f = () => { return "koko"; }
+  console.log((f()) -> "koko"
+
+  если return нет то вернется undefined.
+ */
+
+
 const app = express();
 
-  app.post("/", function(request,response) {
+  app.post("/", function(request,response) { // <- функция
     console.log("server on");
 
     let urlRequest = url.parse(request.url, true);
@@ -33,7 +61,9 @@ const app = express();
     // continue <- выйдет из for без прерывания цикла (это тоже попозже)
     //
     // тебе росле после кажлого end нужно завершая выполнение функции
+
     response.end("no");
+
     });
 
 app.listen(3000);

5a914be70451dafa696eb4f6b0fefeab91eb5c6b
Sav2974 <61123131+Sav2974@users.noreply.github.com>
2020-05-13 18:25:33 +0300

    Create app6.js

---
+++ app6.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const url = require("url");
+const _ = require("lodash");
+
+const app = express();
+
+  app.post("/", function(request,response) {
+    console.log("server on");
+
+    let urlRequest = url.parse(request.url, true);
+    const a = (urlRequest.query);
+    console.log(a);
+
+    const parsed = _.parseInt(a, base);
+
+    if (_.isNumber(parsed) === true) {
+
+      response.end("yes")
+    }
+    response.end("no");
+    });
+
+app.listen(3000);
